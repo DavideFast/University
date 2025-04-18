c20c126e116107c1c91622319b35e5dcd5fd7558
DavideFast <davidefast99@gmail.com>
2025-04-18 12:13:11 +0000

    Commenti aggiunti

--- Latency analizer/tcp_user.bpf.c
+++ Latency analizer/tcp_user.bpf.c
@@ -23,7 +23,7 @@ struct tcp_header_reader{
     __u8 kind;
  };
 
-struct __attribute__((__packed__)) tcp_header_timestamps{ //Il packed serve per evitare che la struttura venga formattata ed evitare che non salvi i file
+struct __attribute__((__packed__))  tcp_header_timestamps{ //Il packed serve per evitare che la struttura venga formattata ed evitare che non salvi i file
     __u8 kind;
     __u8 length;
     __u32 tval;
@@ -49,6 +49,7 @@ struct number_ack_map{
    __uint(max_entries,1024);
    __type(key,__u32);
    __type(value,__u32);
+   __uint(pinning, LIBBPF_PIN_BY_NAME);
 } number_ack_map SEC (".maps");
 
 struct dimension_packet_map {
@@ -64,31 +65,32 @@ int be32_to_int(__u8 value){
 
 // Helper function to check if the packet is TCP
 static bool is_tcp(struct ethhdr *eth, void *data_end){
-    // bpf_printk("Il pacchetto contiene...");
+
     // Ensure Ethernet header is within bounds
-    if ((void *)(eth + 1) > data_end)
+    if ((void *)(eth + 1) > data_end){
+   	bpf_printk("Fuori eth bound"); 
         return false;
+    }
 
     // Only handle IPv4 packets
     if (bpf_ntohs(eth->h_proto) != ETH_P_IP){
+	bpf_printk("Pacchetto non IPv4");
         return false;
     }
-    else
-	bpf_printk("Non è un pacchetto IPv4");
-
     struct iphdr *ip = (struct iphdr *)(eth + 1);
 
     // Ensure IP header is within bounds
-    if ((void *)(ip + 1) > data_end)
+    if ((void *)(ip + 1) > data_end){
+	bpf_printk("Fuori ip bound");
         return false;
-
+    }
     // Check if the protocol is TCP
     if (ip->protocol != IPPROTO_TCP){
-        bpf_printk("Questo non è un pacchetto tcp");
+        bpf_printk("Pacchetto non TCP");
 	return false;
     }
     else
-	bpf_printk("Questo è un pacchetto tcp");
+	bpf_printk("Pacchetto TCP");
 
     return true;
 }
@@ -96,15 +98,16 @@ static bool is_tcp(struct ethhdr *eth, void *data_end){
 SEC("xdp")
 int xdp_pass(struct xdp_md *ctx)
 {
+    bpf_printk("_________________________________");
     // Pointers to packet data
     void *dataa = (void *)(long)ctx->data;
     void *dataa_end = (void *)(long)ctx->data_end;
     int inizio = ctx -> data;
-    bpf_printk("L'inizio del pacchetto si colloca a %d",inizio);
+    bpf_printk("Inizio pacchetto: %u",inizio);
     int fine = ctx -> data_end;
     int lengthPacket;
     lengthPacket = fine - inizio;
-    bpf_printk("La lunghezza del pacchetto è %d", &lengthPacket);
+    bpf_printk("Lunghezza pacchetto: %u", lengthPacket);
 
     // Parse Ethernet header
     struct ethhdr *eth = dataa;
@@ -113,8 +116,8 @@ int xdp_pass(struct xdp_md *ctx)
     if (!is_tcp(eth, dataa_end)) {
         return XDP_PASS;
     }
-
-    bpf_printk("Il pacchetto ip inizia a %d", eth + 1);
+     
+    bpf_printk("Il pacchetto IP inizia a %u", eth + 1);
 
     // Cast to IP header
     struct iphdr *ip = (struct iphdr *)(eth + 1);
@@ -127,13 +130,14 @@ int xdp_pass(struct xdp_md *ctx)
 
     // Ensure IP header is within packet bounds
     if ((void *)ip + ip_hdr_len > dataa_end) {
+	bpf_printk("IP fuori bound");
         return XDP_PASS;
     }
 
     // Parse TCP header
     struct tcphdr *tcp = (struct tcphdr *)((unsigned char *)ip + ip_hdr_len);
 
-    bpf_printk("Il pacchetto tcp inizia a %d", (unsigned char *)ip + ip_hdr_len);
+    bpf_printk("Il pacchetto TCP inizia a %u", (unsigned char *)ip + ip_hdr_len);
     
 
     // Ensure TCP header is within packet bounds
@@ -141,34 +145,59 @@ int xdp_pass(struct xdp_md *ctx)
         return XDP_PASS;
     }
 
+
     // Define the number of bytes you want to capture from the TCP header
     int offset = tcp -> doff;
     const int tcp_header_bytes = offset * 4;
+    if((void *)(unsigned char*)tcp+tcp_header_bytes>dataa_end)
+	return XDP_PASS;
+    bpf_printk("Il pacchetto TCP termina a %u", (unsigned char *)tcp + tcp_header_bytes);
 
     int prova=0;
     int count =0;
     bool lock = false;
     struct tcp_header_reader *appoggio;
 
-    while(prova!=8 && count<8 && (void *)((unsigned char *)tcp + 20 + count)<dataa_end){
-        appoggio = (struct tcp_header_reader *)((unsigned char *)tcp + 20 + count);
+    while(prova!=8 && count<8 && ((void *) ((unsigned char *)tcp + 20 + count))<dataa_end){
+	appoggio = (struct tcp_header_reader *)((unsigned char *)tcp +20 +count);
+	bpf_printk("Dentro il loop: %u, count: %d", (unsigned char *)tcp + 20 +count, count);
+	bpf_printk("Dentro il loop diretto %u", (unsigned char *)appoggio);
         prova = appoggio -> kind;
         if(prova == 8)
             lock = true;
         count = count + 1;
     }
+    if(count != 0)
+    	count = count - 1;
+    bpf_printk("%d", count); 
+    bpf_printk("Data end: %u", dataa_end);
+    bpf_printk("Dopo loop: %u",(void *) ((unsigned char *)tcp + 20 + count ));
+    bpf_printk("Dopo loop diretto: %u", (void *)((unsigned char *)appoggio));
+    
+    bpf_printk("Finally: %s", appoggio);
 
-    if(lock){
-        count = count - 1;
+    if(lock && (void *)((unsigned char *)tcp + 20 + count)<=dataa_end){
+	bpf_printk("-------------------------------------");
         struct tcp_header_timestamps *options = (struct tcp_header_timestamps *)((unsigned char *)tcp + 20 + count);
-        bpf_printk("Kind: %d", options -> kind);
-        bpf_printk("Length %d", options -> length);
-        bpf_printk("Tval: %u", bpf_ntohl(options -> tval));
-        bpf_printk("Tsecr: %u", bpf_ntohl(options -> tsecr));
+	unsigned int tval = bpf_ntohl(options->tval);
+	unsigned int tsecr = bpf_ntohl(options->tsecr);	
+	bpf_printk("Inizio options: %u", (void *)(unsigned char*)options);
+	bpf_printk("Options piu dimensione options: %u", (void *)((unsigned char *)options + sizeof(options)));
+	bpf_printk("Dimensione options: %u",sizeof(options));
+	bpf_printk("Dimensione kind: %u", sizeof(options->kind));
+	bpf_printk("Dimensione length: %u", sizeof(options->length));
+	bpf_printk("Dimensione tval: %u", sizeof(options->tval));
+	bpf_printk("Dimensione tsecr: %u", sizeof(options->tsecr));
+	bpf_printk("Options seconda modalita: %u", (void *)((unsigned char *)tcp + 20 + count));
+        //bpf_printk("Kind: %d", options -> kind);
+        //bpf_printk("Length %d", options -> length);
+        //bpf_printk("Tval: %u", tval);
+	//bpf_printk("Tval indirizzo %u", &options -> tval);
+        //bpf_printk("Tsecr: %u", tsecr);
         bpf_printk("IP: %pI4", &ip -> saddr);
         bpf_printk("Seq: %u", bpf_ntohl(tcp -> seq));
         bpf_printk("Ack: %u", bpf_ntohl(tcp -> ack));
-        bpf_printk("_________________________________");
+        bpf_printk("----------------------------------------------");
     }
 
 
@@ -186,8 +215,14 @@ int xdp_pass(struct xdp_md *ctx)
     // Copy the TCP header bytes into the ring buffer
     // Using a loop to ensure compliance with eBPF verifier
     for (int i = 0; i < 20; i++) {
-        unsigned char byte = *((unsigned char *)tcp + i);
-	((unsigned char *)ringbuf_space)[i] = byte;
+	if(tcp_header_bytes<=20){
+        	unsigned char byte = *((unsigned char *)tcp + i);
+		((unsigned char *)ringbuf_space)[i] = byte;
+	}
+	else{
+		unsigned char byte = 00000000;
+        	((unsigned char *)ringbuf_space)[i] = byte;
+        }
     }
 
     int key = ip->saddr;
@@ -198,9 +233,9 @@ int xdp_pass(struct xdp_md *ctx)
     long *value = bpf_map_lookup_elem(&inner_map, &key);
     long *value_destination = bpf_map_lookup_elem(&inner_map,&keyd);
     long init = 0;
-    bpf_printk("Risultato lookup %d",value);
-    bpf_printk("Risultato lookup %d", &value);
-    bpf_printk("Indirizzo destinatario %pI4",&keyd);
+    //bpf_printk("Risultato lookup %d",value);
+    //bpf_printk("Risultato lookup %d", &value);
+    //bpf_printk("Indirizzo destinatario %pI4",&keyd);
     if(value){
         long latency = current_time - *value;
 	bpf_map_update_elem(&inner_map,&key,&current_time,BPF_ANY);
@@ -218,7 +253,7 @@ int xdp_pass(struct xdp_md *ctx)
 
     bpf_ringbuf_submit(ringbuf_space, 0);
     bpf_printk("La fine del pacchetto si colloca a %d", ctx -> data_end);
-
+    bpf_printk("________________________________________________________");
     return XDP_PASS;
 }
 
