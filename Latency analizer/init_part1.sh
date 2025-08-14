4e6d1d198de9bf02bc9aa4052b3b71ed1302503e
DavideFast <davidefast99@gmail.com>
2025-08-14 17:56:41 +0000

    Init of server

---
+++ Latency analizer/init_part1.sh
@@ -0,0 +1,6 @@
+cp usr/src/linux-headers-6.8.0-45-generic/tools/bpf/resolve_btfids/libbpf/include/bpf /usr/include
+sudo apt-get update
+sudo apt install libbpf-dev
+sudo apt install clang
+echo Download from DavideFast GitHub Account the Latency Analyzer kit
+
---
+++ Latency analizer/init_part2.sh
@@ -0,0 +1,4 @@
+sudo gcc -o user_space_program user_space_program.c -lbpf
+sudo clang -target bpf -g -O2 -Wall -v -c tcp_user.bpf.c -o tcp_user.bpf.o
+sudo clang -target bpf -g -O2 -Wall -v -c tcp.bpf.c -o tcp.bpf.o
+sudo ./user_space_program
