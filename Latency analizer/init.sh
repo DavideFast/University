4a36e7622b53efd676292666dec2b3c948c3b9d6
DavideFast <davidefast99@gmail.com>
2025-08-14 18:16:59 +0000

    Init update

--- Latency analizer/init.sh
+++ Latency analizer/init.sh
@@ -1,13 +1,19 @@
+echo Installazione delle dipendenze
 cp usr/src/linux-headers-6.8.0-45-generic/tools/bpf/resolve_btfids/libbpf/include/bpf /usr/include
 sudo apt-get update
 sudo apt install libbpf-dev
 sudo apt install clang
+
 echo Download from DavideFast GitHub Account the Latency Analyzer kit
 wget https://github.com/DavideFast/University/tree/4e6d1d198de9bf02bc9aa4052b3b71ed1302503e/Latency%20analizer/tcp_user.bpf.c
 wget https://github.com/DavideFast/University/tree/4e6d1d198de9bf02bc9aa4052b3b71ed1302503e/Latency%20analizer/tcp.bpf.c
 wget https://github.com/DavideFast/University/tree/4e6d1d198de9bf02bc9aa4052b3b71ed1302503e/Latency%20analizer/user_space_program.c
+wget https://github.com/DavideFast/University/tree/4e6d1d198de9bf02bc9aa4052b3b71ed1302503e/Latency%20analizer/termina.sh
+wget https://github.com/DavideFast/University/tree/4e6d1d198de9bf02bc9aa4052b3b71ed1302503e/Latency%20analizer/vmlinux.h
+
 sudo gcc -o user_space_program user_space_program.c -lbpf
 sudo clang -target bpf -g -O2 -Wall -v -c tcp_user.bpf.c -o tcp_user.bpf.o
 sudo clang -target bpf -g -O2 -Wall -v -c tcp.bpf.c -o tcp.bpf.o
+
 echo Avvio del programma
 sudo ./user_space_program
