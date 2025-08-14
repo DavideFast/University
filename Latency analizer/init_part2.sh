4f30275e6208e458ade949e914f56e907df60d11
DavideFast <davidefast99@gmail.com>
2025-08-14 18:13:00 +0000

    Delete Latency analizer/init_part2.sh

--- Latency analizer/init_part2.sh
+++
@@ -1,4 +0,0 @@
-sudo gcc -o user_space_program user_space_program.c -lbpf
-sudo clang -target bpf -g -O2 -Wall -v -c tcp_user.bpf.c -o tcp_user.bpf.o
-sudo clang -target bpf -g -O2 -Wall -v -c tcp.bpf.c -o tcp.bpf.o
-sudo ./user_space_program
