80b51e1047e2b8f90ff923c8fc1e707ada43679f
DavideFast <davidefast99@gmail.com>
2025-08-17 11:52:27 +0200

    Update update.sh

--- Latency analizer/update.sh
+++ Latency analizer/update.sh
@@ -1,3 +1,3 @@
-sudo clang -target bpf -g -O2 -Wall -v -c ingress_tcp.bpf.c -o ingress_tcp.bpf.o
-sudo clang -target bpf -g -O2 -Wall -v -c egress_tcp.bpf.c -o egress_tcp.bpf.o
+sudo clang -target bpf -g -O2 -Wall -v -c ingress_tcp_method2.bpf.c -o ingress_tcp_method2.bpf.o
+sudo clang -target bpf -g -O2 -Wall -v -c egress_tcp_method2.bpf.c -o egress_tcp_method2.bpf.o
 sudo gcc -o user_space_program user_space_program.c -lbpf
