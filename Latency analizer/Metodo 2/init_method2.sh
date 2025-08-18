0c57532b3f23a4856ee7b72496d78e3cff3d083d
DavideFast <davidefast99@gmail.com>
2025-08-18 20:30:28 +0000

    Add files via upload

--- Latency analizer/Metodo 2/init_method2.sh
+++ Latency analizer/Metodo 2/init_method2.sh
@@ -12,7 +12,7 @@ wget https://raw.githubusercontent.com/DavideFast/University/refs/heads/main/Lat
 wget https://raw.githubusercontent.com/DavideFast/University/refs/heads/main/Latency%20analizer/Metodo%202/user_space_program_method2.c
 wget https://raw.githubusercontent.com/DavideFast/University/refs/heads/main/Latency%20analizer/Metodo%202/update_method2.sh
 
-sudo gcc -o user_space_program user_space_program_method2.c -lbpf
+sudo gcc -o user_space_program_method2 user_space_program_method2.c -lbpf
 sudo clang -target bpf -g -O2 -Wall -v -c egress_tcp_method2.bpf.c -o egress_tcp_method2.bpf.o
 sudo clang -target bpf -g -O2 -Wall -v -c ingress_tcp_method2.bpf.c -o ingress_tcp_method2.bpf.o
 
