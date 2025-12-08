ee8a7094bb9f01b8d30f3a6eb7827e539daa10e7
DavideFast <davidefast99@gmail.com>
2025-12-08 22:46:18 +0100

    Update

--- PingTime/frontend/src/Prova.js
+++ PingTime/frontend/src/Prova.js
@@ -1,7 +1,12 @@
 import React, { useEffect } from 'react';
 import {App} from './App.js';
+import {App2} from './Grafico1/Grafico.js';
 
 
 export default function Home() {
-    return (<App />);
+    return (
+      <>
+        <App />
+        <App2 />
+      </>);
   } 
